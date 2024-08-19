b6a2c22e950112b06ad120c0395021d0ca0f267c
Jesse <67044022+J3S5E@users.noreply.github.com>
2024-08-19 17:48:31 +0800

    feat: del harpoon mark mapping

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -9,6 +9,7 @@ return {
 
     keymap.set('n', '<leader>hl', '<cmd>Telescope harpoon marks<cr>', { desc = '[h]arpoon marks [l]ist' })
     keymap.set('n', '<leader>hh', "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = 'Mark file with [h]arpoon' })
+    keymap.set('n', '<leader>hd', "<cmd>lua require('harpoon.mark').rm_file()<cr>", { desc = '[h]arpoon mark - [d]elete' })
     keymap.set('n', '<leader>hp', "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = '[h]arpoon mark - [p]revious' })
     keymap.set('n', '<leader>hn', "<cmd>lua require('harpoon.ui').nav_prev()<cr>", { desc = '[h]arpoon mark - [n]ext' })
   end,
